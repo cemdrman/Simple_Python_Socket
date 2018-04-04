793ea916d5555d4456c3a874646751022fd011fd
cem dırman <cemdrman@gmail.com>
2018-04-04 22:31:03 +0300

    modified

--- client.py
+++ client.py
@@ -5,21 +5,15 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-ip_address = "172.16.17.154"
-socket_port = 3000
+ip_address = "0.0.0.0"
+socket_port = 3008
 server_address = (ip_address,socket_port)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:    
-    # Look for the response
-    message = "00122223333"
-    amount_received = 0
-    amount_expected = len(message)
-    while True:
-       
-        data = sock.recv(16)
-        amount_received += len(data)
+    while True:       
+        data = sock.recv(1024)
         print >>sys.stderr, 'received %s' % data
 
 finally:
